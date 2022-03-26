f8708a3e343de72b4f4218087b7ca5fb58267780
Kayky Freitas <kayky_defreitas@hotmail.com>
2022-03-25 23:00:54 -0300

    Iniciando o desenvolvimento do player

---
+++ PlayerPage/main.js
@@ -0,0 +1,93 @@
+let musics = [
+    {
+        title: 'Malavdão 3',
+        artist: 'Xamã',
+        path: 'Audios/malvadao3.mp3',
+        image: 'Assets/MusicBanner/malvadao3.svg',
+    },
+    {
+        title: 'Deus é meu guia',
+        artist: 'Chefin',
+        path: 'Audios/deusEMeuGuia.mp3',
+        image: 'Assets/MusicBanner/deusEMeuGuia.svg',
+    },
+    {
+        title: 'Pandora',
+        artist: 'Matt-D',
+        path: 'Audios/pandora.mp3',
+        image: 'Assets/MusicBanner/pandora.svg',
+    },
+]
+
+
+let music = document.querySelector('audio');
+let musicIndex = 0;
+
+let musicDuration = document.querySelector('.totalTime');
+let musicBanner = document.querySelector('.musicBanner');
+let musicName = document.querySelector('.about h1');
+let musicArtist = document.querySelector('.about h2');
+
+musicRender(musicIndex);
+
+//Events
+document.querySelector('.play').addEventListener('click', playMusic);
+document.querySelector('.pause').addEventListener('click', pauseMusic);
+
+music.addEventListener('timeupdate', barAtualize);
+
+document.querySelector('.prev').addEventListener('click', () => {
+    musicIndex--;
+    if(musicIndex < 0) {
+        musicIndex = 2;
+    }
+    musicRender(musicIndex);
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+    musicIndex++;
+    if(musicIndex > 2) {
+        musicIndex = 0;
+    }
+    musicRender(musicIndex);
+})
+
+//Functions
+
+function musicRender(index) {
+    music.setAttribute('src', musics[index].path);
+    music.addEventListener('loadeddata', () => {
+        musicName.textContent = musics[index].title;
+        musicArtist.textContent = musics[index].artist;
+        musicBanner.src = musics[index].image;
+        musicDuration.textContent = timeConvert(Math.floor(music.duration));
+    });
+}
+
+function playMusic() {
+    music.play();
+    document.querySelector('.pause').style.display = 'block';
+    document.querySelector('.play').style.display = 'none';
+}
+
+function pauseMusic() {
+    music.pause();
+    document.querySelector('.pause').style.display = 'none';
+    document.querySelector('.play').style.display = 'block';
+}
+
+function barAtualize() {
+    let bar = document.querySelector('progress');
+    bar.style.width = Math.floor((music.currentTime / music.duration) * 100) + '%';
+    let curTime = document.querySelector('.currentTime');
+    curTime.textContent = timeConvert(Math.floor(music.currentTime));
+}
+
+function timeConvert(seconds) {
+    let minutesArea = Math.floor(seconds / 60);
+    let secondsArea = seconds % 60;
+    if(secondsArea < 10) {
+        secondsArea = '0' + secondsArea;
+    }
+    return `${minutesArea} : ${secondsArea}`;
+}
