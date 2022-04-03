70e166729acfb1e3cb5618c9dd15537777d45710
Kayky Freitas <kayky_defreitas@hotmail.com>
2022-04-02 23:26:56 -0300

    Criando Playlist1

---
+++ Scripts/Playlist1Page/data.js
@@ -0,0 +1,58 @@
+const musics = [
+    {
+        title: 'Bring Me The Light',
+        artist: 'Mara Necia, Rob Gasser, T & Sugah',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/108/bring-me-the-light-rob-gasser-remix-1648720836-FnfbgRC4Ly.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/beb37788-dca5-4b51-8de1-2fa79a5efd74/artwork-440x440.jpg',
+    },
+    {
+        title: 'In My Head',
+        artist: 'Britt Lari, Arcando',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/110/in-my-head-1648807239-R6dTi5vqkn.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/54cf1209-cdbf-4686-8a98-c2b47322c845/artwork-440x440.jpg',
+    },
+    {
+        title: 'Complicated',
+        artist: 'WBN, WIBERG',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/104/complicated-1647950438-oKhVEXpaLA.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/0c440595-6e72-454b-ab29-42dba2205e25/artwork-440x440.jpg',
+    },
+    {
+        title: 'Fall In Love',
+        artist: 'DigEx',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/101/fall-in-love-1647360038-hsnRvacMl6.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/0230a245-8aae-4a3d-a2ed-844055a11b7c/artwork-440x440.jpg',
+    },
+    {
+        title: 'With You',
+        artist: 'OBLVYN, Riell',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/099/with-you-1646917239-Xm9KNcQNZn.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/55cb6b0f-04f3-430a-99f7-a07a66c70f26/artwork-440x440.jpg',
+    },
+    {
+        title: 'Freedom',
+        artist: 'Goodknight',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/095/freedom-1646139639-w40yOUD1MG.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/ed152ae5-4951-4aae-8ae2-acb44a407947/artwork-440x440.jpg',
+    },
+    {
+        title: 'Let Me Go',
+        artist: 'Jimmy Rivler, Max Vermeulen, Almost Weekend',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/073/let-me-go-1641902437-UxXbdyBdUo.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/ea2fde6f-faac-4321-b67f-db7b0ff8d404/artwork-440x440.jpg',
+    },
+    {
+        title: 'Trust Me',
+        artist: 'DigEx',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/067/trust-me-1640685637-pNBK304N3L.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/fe26a5d6-9de0-4b00-b02d-3819fb5ca39b/artwork-440x440.jpg',
+    },
+    {
+        title: 'Hate You',
+        artist: 'Nito-Onna, BAUWZ, Poylow',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/057/hate-you-1639738837-t3bnjQUuNm.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/34048c64-4781-4976-a755-6fb3fcf4436b/artwork-440x440.jpg',
+    },
+]
+
+export { musics };
