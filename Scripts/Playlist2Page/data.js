4d932d64bc25ea56744961c562ec3bddc48a752c
Kayky Freitas <kayky_defreitas@hotmail.com>
2022-04-04 22:49:22 -0300

    Adicionando playlist2

---
+++ Scripts/Playlist2Page/data.js
@@ -0,0 +1,58 @@
+const musics = [
+    {
+        title: 'Cradles',
+        artist: 'Mara Necia, Rob Gasser, T & Sugah',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/577/cradles-1586957268-RxHgjX9e14.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/64042cef-adb4-4c2b-a602-c63f74d80bcd/artwork-440x440.jpg',
+    },
+    {
+        title: 'Haunted House',
+        artist: 'Britt Lari, Arcando',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/100/haunted-house-1647097237-HAGlYrFRLn.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/dec2f349-8391-4f15-88ae-1b3380b82550/artwork-440x440.jpg',
+    },
+    {
+        title: 'Circus',
+        artist: 'WBN, WIBERG',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/080/circus-1643112038-TNodCSU1cJ.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/156a493c-027c-44f5-a6c9-eb8fd716c1f8/artwork-440x440.jpg',
+    },
+    {
+        title: 'Im Fine',
+        artist: '3rd Prototype',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/788/im-fine-1597319464-qnKkHgCCC0.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/5ee1f835-297d-421d-8f53-f90b9a60d75f/artwork-440x440.jpg',
+    },
+    {
+        title: 'Donuts',
+        artist: '2SCOOPS',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/497/donuts-1586954855-EiRIEDSPqC.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/5d58b2b0-2459-4054-9320-53d90d64f8d6/artwork-440x440.jpg',
+    },
+    {
+        title: 'Freedom',
+        artist: 'Goodknight',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/095/freedom-1646139639-w40yOUD1MG.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/ed152ae5-4951-4aae-8ae2-acb44a407947/artwork-440x440.jpg',
+    },
+    {
+        title: 'Incomplete',
+        artist: 'Aero Chord, Anuka, T-Mass',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/419/incomplete-t-mass-remix-1586952746-j5LbKvhWee.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/1a90f7af-15dd-46b8-8ffa-80557a78c4ce/artwork-440x440.jpg',
+    },
+    {
+        title: 'The Return',
+        artist: 'NIVIRO',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/404/the-return-1586952223-NULOzbjiUH.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/53273ff8-773a-4b81-9bf5-3d167409c1be/artwork-440x440.jpg',
+    },
+    {
+        title: 'Hate You',
+        artist: 'Nito-Onna, BAUWZ, Poylow',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/057/hate-you-1639738837-t3bnjQUuNm.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/34048c64-4781-4976-a755-6fb3fcf4436b/artwork-440x440.jpg',
+    },
+]
+
+export { musics };
